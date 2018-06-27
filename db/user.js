ce750f9da27c737cb98435a9bb1da4dbb40d86ec
KmKavya <kavyavshaiva@gmail.com>
2018-06-27 19:18:55 +0530

    used bcryptjs to generate saltString and hash

--- db/user.js
+++ db/user.js
@@ -1,9 +1,10 @@
 const debug = require("debug")("evolvus-user:db:user");
 const mongoose = require("mongoose");
 const ObjectId = require('mongodb')
-    .ObjectID;
+  .ObjectID;
 
 const userSchema = require("./userSchema");
+const bcrypt = require('bcryptjs');
 
 // Creates a userCollection collection in the database
 var userCollection = mongoose.model("userCollection", userSchema);
@@ -11,31 +12,42 @@ var userCollection = mongoose.model("userCollection", userSchema);
 // Saves the userCollection object to the database and returns a Promise
 // The assumption here is that the Object is valid
 module.exports.save = (object) => {
-    return new Promise((resolve, reject) => {
-        try {
-            // any exception during construction will go to catch
-            let user = new userCollection(object);
-            // on resolve we need to resolve the this method
-            // on reject or exception we reject it,
-            // this is because the record either saves or it doesnt
-            // in any case it does not save, is a reject
-            user.save()
-                .then((data) => {
-                    debug("saved successfully", data._id);
-                    resolve(data);
-                }, (err) => {
-                    debug(`rejected save.. ${err}`);
-                    reject(err);
-                })
-                .catch((e) => {
-                    debug(`exception on save: ${e}`);
-                    reject(e);
-                });
-        } catch (e) {
-            debug(`caught exception: ${e}`);
-            reject(e);
-        }
-    });
+  return new Promise((resolve, reject) => {
+    try {
+      // any exception during construction will go to catch
+      bcrypt.genSalt(10, function (err, salt) {
+        bcrypt.hash(object.userPassword, salt, function (err, hash) {
+          // Assign hashedPassword to your userPassword and store it in DB.
+          object.userPassword = hash;
+          object.saltString = salt;
+          let user = new userCollection(object);
+          // on resolve we need to resolve this method
+          // on reject or exception we reject it,
+          // this is because the record either saves or it doesnt
+          // in any case it does not save, is a reject
+          user.save()
+            .then((data) => {
+              data = data.toObject();
+              delete data.userPassword;
+              delete data.saltString;
+              debug("saved successfully", data);
+              resolve(data);
+            }, (err) => {
+              debug(`rejected save.. ${err}`);
+              reject(err);
+            })
+            .catch((e) => {
+              debug(`exception on save: ${e}`);
+              reject(e);
+            });
+        });
+      });
+
+    } catch (e) {
+      debug(`caught exception: ${e}`);
+      reject(e);
+    }
+  });
 };
 
 
@@ -43,10 +55,10 @@ module.exports.save = (object) => {
 // if the collectiom has no records it Returns
 // a promise with a result of  empty object i.e. {}
 module.exports.findAll = (limit) => {
-    if (limit < 1) {
-        return userCollection.find({});
-    }
-    return userCollection.find({}).limit(limit);
+  if (limit < 1) {
+    return userCollection.find({});
+  }
+  return userCollection.find({}).limit(limit);
 };
 
 // Finds the user which matches the value parameter from user collection
@@ -54,27 +66,27 @@ module.exports.findAll = (limit) => {
 // null, undefined should be rejected with Invalid Argument Error
 // Should return a Promise
 module.exports.findOne = (attribute, value) => {
-    return new Promise((resolve, reject) => {
-        try {
-            var query = {};
-            query[attribute] = value;
-            userCollection.findOne(query)
-                .then((data) => {
-                    debug(`user found ${data}`);
-                    resolve(data);
-                }, (err) => {
-                    debug(`rejected find.. ${err}`);
-                    reject(err);
-                })
-                .catch((e) => {
-                    debug(`exception on find: ${e}`);
-                    reject(e);
-                });
-        } catch (e) {
-            debug(`caught exception: ${e}`);
-            reject(e);
-        }
-    });
+  return new Promise((resolve, reject) => {
+    try {
+      var query = {};
+      query[attribute] = value;
+      userCollection.findOne(query)
+        .then((data) => {
+          debug(`user found ${data}`);
+          resolve(data);
+        }, (err) => {
+          debug(`rejected find.. ${err}`);
+          reject(err);
+        })
+        .catch((e) => {
+          debug(`exception on find: ${e}`);
+          reject(e);
+        });
+    } catch (e) {
+      debug(`caught exception: ${e}`);
+      reject(e);
+    }
+  });
 };
 
 // Finds all the users which matches the value parameter from user collection
@@ -82,27 +94,27 @@ module.exports.findOne = (attribute, value) => {
 // null, undefined should be rejected with Invalid Argument Error
 // Should return a Promise
 module.exports.findMany = (attribute, value) => {
-    return new Promise((resolve, reject) => {
-        try {
-            var query = {};
-            query[attribute] = value;
-            userCollection.find(query)
-                .then((data) => {
-                    debug(`user found ${data}`);
-                    resolve(data);
-                }, (err) => {
-                    debug(`rejected find.. ${err}`);
-                    reject(err);
-                })
-                .catch((e) => {
-                    debug(`exception on find: ${e}`);
-                    reject(e);
-                });
-        } catch (e) {
-            debug(`caught exception: ${e}`);
-            reject(e);
-        }
-    });
+  return new Promise((resolve, reject) => {
+    try {
+      var query = {};
+      query[attribute] = value;
+      userCollection.find(query)
+        .then((data) => {
+          debug(`user found ${data}`);
+          resolve(data);
+        }, (err) => {
+          debug(`rejected find.. ${err}`);
+          reject(err);
+        })
+        .catch((e) => {
+          debug(`exception on find: ${e}`);
+          reject(e);
+        });
+    } catch (e) {
+      debug(`caught exception: ${e}`);
+      reject(e);
+    }
+  });
 };
 
 // Finds the user for the id parameter from the user collection
@@ -111,59 +123,61 @@ module.exports.findMany = (attribute, value) => {
 // All returns are wrapped in a Promise
 //
 module.exports.findById = (id) => {
-    return new Promise((resolve, reject) => {
-        try {
-            userCollection.findById({
-                    _id: new ObjectId(id)
-                })
-                .then((res) => {
-                    debug("findById successfull: ", res);
-                    resolve(res);
-                }, (err) => {
-                    debug(`rejected finding userCollection.. ${err}`);
-                    reject(err);
-                })
-                .catch((e) => {
-                    debug(`exception on finding user: ${e}`);
-                    reject(e);
-                });
-        } catch (e) {
-            debug(`caught exception: ${e}`);
-            reject(e);
-        }
-    });
+  return new Promise((resolve, reject) => {
+    try {
+      userCollection.findById({
+        _id: new ObjectId(id)
+      })
+        .then((res) => {
+          debug("findById successfull: ", res);
+          resolve(res);
+        }, (err) => {
+          debug(`rejected finding userCollection.. ${err}`);
+          reject(err);
+        })
+        .catch((e) => {
+          debug(`exception on finding user: ${e}`);
+          reject(e);
+        });
+    } catch (e) {
+      debug(`caught exception: ${e}`);
+      reject(e);
+    }
+  });
 };
 
 
 //Authenticate User credentials {userName,userPassword,application}
 module.exports.authenticate = (credentials) => {
-    return new Promise((resolve, reject) => {
-        try {
-            userCollection.find({
-                    userName: credentials.userName,
-                    userPassword: credentials.userPassword,
-                    application.applicationCode: credentials.application
-                })
-                .then((res) => {
-                    debug("authentication successful: ", res);
-                    resolve(res);
-                }, (err) => {
-                    debug(`Invalid Credentials. ${err}`);
-                    reject(err);
-                })
-                .catch((e) => {
-                    debug(`exception on authenticating user: ${e}`);
-                    reject(e);
-                });
-        } catch (e) {
-            debug(`caught exception: ${e}`);
-            reject(e);
+  return new Promise((resolve, reject) => {
+    try {
+      userCollection.find({
+        userName: credentials.userName,
+        userPassword: credentials.userPassword,
+        application: {
+          applicationCode: credentials.application.applicationCode
         }
-    });
+      })
+        .then((res) => {
+          debug("authentication successful: ", res);
+          resolve(res);
+        }, (err) => {
+          debug(`Invalid Credentials. ${err}`);
+          reject(err);
+        })
+        .catch((e) => {
+          debug(`exception on authenticating user: ${e}`);
+          reject(e);
+        });
+    } catch (e) {
+      debug(`caught exception: ${e}`);
+      reject(e);
+    }
+  });
 };
 
 // Deletes all the entries of the collection.
 // To be used by test only
 module.exports.deleteAll = () => {
-    return userCollection.remove({});
+  return userCollection.remove({});
 };
--- db/userSchema.js
+++ db/userSchema.js
@@ -1,60 +1,3 @@
-<<<<<<< HEAD
-const mongoose = require("mongoose");
-const validator = require("validator");
-const passwordValidator = require("password-validator");
-
-var schema = new passwordValidator();
-schema
-  .is()
-  .min(8)
-  .is()
-  .max(100)
-  .has()
-  .uppercase()
-  .has()
-  .lowercase()
-  .has()
-  .digits()
-  .has()
-  .not()
-  .spaces();
-
-const {
-  branch
-} = require('evolvus-entity');
-var {contact} = require('evolvus-contact');
-var Branch = mongoose.model("branchCollection", branch.branchDBschema);
-var Contact = mongoose.model("contactCollection",contact.contactDBschema);
-
-var userSchema = new mongoose.Schema({
-  // Add all attributes below tenantId
-  tenantId: {
-    type: String,
-    required: true,
-    minLength: 1,
-    maxLength: 64
-  },
-  contact: {
-    type: Object,
-    ref: 'Contact'
-  },
-  branch: {
-    type: Object,
-    ref: 'Branch'
-  },
-  userName: {
-    type: String,
-    required: true
-  },
-  userPassword: {
-    type: String,
-    required: true,
-    validate: {
-      validator: (val) => {
-        return schema.validate(val);
-      },
-      message: "Password must contain atleast one uppercase letter,lowercase,one digit and should not have spaces."
-=======
 const mongoose = require('mongoose');
 const validator = require('validator');
 
@@ -131,37 +74,7 @@ var userSchema = new mongoose.Schema({
     },
     processingStatus: {
         type: String
->>>>>>> fe91db44542790122e80400467f26ae5ec2b12c7
     }
-  },
-  createdBy: {
-    type: String,
-    required: true
-  },
-  updatedBy: {
-    type: String
-  },
-  createdDate: {
-    type: Date,
-    required: true
-  },
-  lastUpdatedDate: {
-    type: Date
-  },
-  enabledFlag: {
-    type: Number,
-    default: 1
-  },
-  deletedFlag: {
-    type: Number,
-    default: 0
-  },
-  activationStatus: {
-    type: String
-  },
-  processingStatus: {
-    type: String
-  }
 
 });
 
